483175e1b5cc9f01d95847ea831aa48d25b485a5
Michael Nguyen <contact@micnguyen.net>
2023-06-01 13:25:33 +1000

    Initial commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,4 @@
 module.exports = {
   root: true,
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint', 'prettier'],
-  extends: [
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended',
-    'prettier'
-  ],
-  rules: {
-    semi: 0
-  },
-  overrides: [
-    {
-      files: ['*.test.tsx', 'src/utils/testUtils.tsx'],
-      rules: {
-        '@typescript-eslint/no-explicit-any': 0,
-        '@typescript-eslint/ban-types': 0
-      }
-    }
-  ]
-}
+  extends: '@react-native-community',
+};
